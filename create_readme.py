803482a22b6b8f7f23aba562b955586d708139a5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-10-23 18:24:07 +0100

    update

--- create_readme.py
+++ create_readme.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 """ Create a readme using Gemini ai """
 
-import google.generativeai
+from google import generativeai as genai
+import os
+import sys
+
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+try:
+    project_name = sys.argv[1]
+except KeyError:
+    print('Please pass the project name as an argument')
+
+prompt = f"""
+Design a detailed README for a project named "{project_name}".
+Include the following sections:
+Explain "{project_name}" in a few sentences.
+1. Project Description
+2. Installation Instructions
+3. Usage Instructions
+4. Contributing Guidelines
+5. License Information
+"""
+
+response = model.generate_content()
+print(response.text)
